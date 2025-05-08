c5c2ad26d47a627ecddd0b68921b9f412ef39674
muntazirabbas460 <muntazirabbas0347@gmail.com>
2025-05-08 16:45:19 +0500

    python for research

---
+++ python/first.py
@@ -0,0 +1,100 @@
+# name = input("What is your name? ")
+# print("hello " + name)
+
+# name = "aslam"
+# age = 23
+# price = 32.9
+# price2 = price
+# old = True
+# print(type(price))
+# print(type(age))
+# print(type(age))
+# print(type(name))
+# print(type(old))
+
+# sum of 2 numbers
+# a = 3
+# b = 5
+# sum = a+ b
+# print(sum)
+
+
+# name = input("name ")
+# age = int(input("age "))
+# # price = float(input("price "))
+
+# print("my name is", name,  "and I am", age, "years old")
+
+
+
+# x = float(input("what is x? "))
+# y = float(input("what is y? "))
+
+# z = round(x + y)
+# print(f"{z:,}")
+
+# taking Input froim user
+
+# name = input("name :")
+# print(name)
+
+# prize = int(input("price :"))
+
+# age = int(input("age :"))
+
+# print("My name is ", name, "age is ", age , "years old", "price ", prize) 
+ 
+
+# light = input("light Color :")
+
+# if(light == "red"):
+#     print("stop")
+# elif(light == "yellow"):
+#     print("look") 
+# elif(light =="green"):
+#     print(" goo")
+# else:
+#     print("light is brokrn")                       
+
+
+# light = input(light ="Light color")
+# if(light =="red"):
+#     print("stop")
+# elif(light =="green "):
+#     print("go")
+# elif(light =="yellow"):
+#     print("look")
+# else:
+#     print("brokrn")
+
+
+# light = input("light color :")
+# if (light =="red"):
+#     print("stop")
+# elif(light =="green"):
+#     print("go")
+# elif(light ==("yellow")):
+#     print("look")
+# else:
+#     print("light is broken")                          
+
+
+
+# age = int(input(" vote :"))
+# if (age >= 18):
+#    print("Yes")
+# else:
+#    print("No")                 
+
+# a = int(input("num1 :"))
+# b = int(input(" Num 2 :"))
+# print (a>=b)
+
+
+str = "emergence"
+print(str[1 :7])
+
+
+
+
+
