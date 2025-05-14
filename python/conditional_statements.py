eee7486669f196d457b308878a369413e55fd96c
muntazirabbas460 <muntazirabbas0347@gmail.com>
2025-05-14 12:25:02 +0500

    LISTS

---
+++ python/conditional_statements.py
@@ -0,0 +1,9 @@
+light = "yellow"
+
+if (light == "red"):
+     print("Stop")
+elif(light == "green"):
+     print("go")
+elif(light == "yellow"):
+     print("reday")
+print("out of order")                             
--- python/second_chap.py
+++ python/second_chap.py
@@ -7,18 +7,33 @@
 # str = str[0]
 # print(str)
 
-"""
-Slicing
-"""
+# """
+# Slicing
+# """
 
-str = " sab ka collage"
-print(str[1:4])
+# str = " sab ka collage"
+# print(str[1:4])
 
 
-# to get full string
-str =" apna collage"
-print(str[5:len(str)])
+# # to get full string
+# str =" apna collage"
+# print(str[5:len(str)])
 
-str = " apna school"
-print(str[ :5])#[0:4]
+# str = " apna school"
+# print(str[ :5])#[0:4]
+
+
+
+
+
+
+# write a programme to get the user name as input
+# from user and get print its length
+
+# name = input("enter the user name :")
+# print("length of string is ", len(name))
+
+
+name = input("name of user :")
+print("length of string ", len[name])
 
