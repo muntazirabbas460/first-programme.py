db6dd8d24902f44fbc39bc451e6a259dbe34f2ed
muntazirabbas460 <muntazirabbas0347@gmail.com>
2025-05-20 16:10:57 +0500

    Loops

--- python/Chap_3_lists.py
+++ python/Chap_3_lists.py
@@ -1,54 +1,54 @@
 # lists and tuples
 
-# marks = [94.5, 78.7, 66.4, 29.1]
-# print(marks)
-# print(type(marks))
-# print(len(marks))
-# print(marks[0])
-# print(marks[3])
+marks = [94.5, 78.7, 66.4, 29.1]
+print(marks)
+print(type(marks))
+print(len(marks))
+print(marks[0])
+print(marks[3])
 
 
-# student = ["asla,", "akram", 78.5, 55.7]
-# print(student)
+student = ["asla,", "akram", 78.5, 55.7]
+print(student)
 
 
 # LIST METHODS
 # (1) append method
-# list = [4, 7, 8, 9]
-# list.append(3)
-# print(list)
+list = [4, 7, 8, 9]
+list.append(3)
+print(list)
 
-# list = [4, 5, 6, 9]
-# list.append(10)
-# print(list)
+list = [4, 5, 6, 9]
+list.append(10)
+print(list)
 
 
 # (2) list Sort method
 
-# list = [10, 51, 8, 9]
-# print(list.append(6))
+list = [10, 51, 8, 9]
+print(list.append(6))
 
 
 # (3) list reverse method
-# print(list.sort(reverse=True))
-# print(list)
+print(list.sort(reverse=True))
+print(list)
 
 
-# list = [34, 67, 88, 9]
-# print(list.append())
-# print(list)
+list = [34, 67, 88, 9]
+print(list.append())
+print(list)
 
 # (4)list reversed mrthod
 
-# list = [2, 3, 4, 6]
-# list.insert(1, 9)
-# print(list)
+list = [2, 3, 4, 6]
+list.insert(1, 9)
+print(list)
 
 
 # (5) list remove method
-# list = [2, 3, 4, 8]
-# list.remove(3)
-# print(list)
+list = [2, 3, 4, 8]
+list.remove(3)
+print(list)
 
 # (6) List.pop Method
 
--- python/Tuples.py
+++ python/Tuples.py
@@ -2,68 +2,68 @@
 # the big difference b/w tuple and lists is that tuple is immutable while list is muttable
 
 
-# tup = (2, 3, 9, 7, 8)
-# print(type(tup))
+tup = (2, 3, 9, 7, 8)
+print(type(tup))
 
-# tup = (2, 88, 9, 0)
-# print(tup[0])
-# print(tup[1])
-
-
-
-
-
-# tup = (2, 88, 9, 0)
-# print(tup[0:2])
+tup = (2, 88, 9, 0)
+print(tup[0])
+print(tup[1])
 
 
 
 
-# tup = (2, 88, 9, 0)
-# print(tup.count(2))
+
+tup = (2, 88, 9, 0)
+print(tup[0:2])
+
+
+
+
+tup = (2, 88, 9, 0)
+print(tup.count(2))
 
 
 
 
 # WAP to ask the user to enter the names of three favourite  movies and store them in lists
 
-# movies = []
+movies = []
 
-# movie1 = input("enter movie 1 ")
-# movie2 = input("enter movie 2 ")
-# movie3 = input("enter movie 3 ")
+movie1 = input("enter movie 1 ")
+movie2 = input("enter movie 2 ")
+movie3 = input("enter movie 3 ")
 
-# movies.append(movie1)
-# movies.append(movie2)
-# movies.append(movie3)
+movies.append(movie1)
+movies.append(movie2)
+movies.append(movie3)
 
-# print(movies)
+print(movies)
 
 
 
 # WAP to check If list contains palindrome of elements. 
-# list1 = [1, 2, 1]
-# list2 = [1, 2, 3]
+list1 = [1, 2, 1]
+list2 = [1, 2, 3]
 
-# copy_list1 = list1.copy()
-# copy_list1.reverse()
+copy_list1 = list1.copy()
+copy_list1.reverse()
 
-# if(copy_list1 == list1):
-#     print("palindrome")  
-# else:
-#     print("Not plaindrome") 
+if(copy_list1 == list1):
+    print("palindrome")  
+else:
+    print("Not plaindrome") 
 
 
-# list1 = ["a", "b", "a"]                   
-# list2 = ["a", "b", "c", "d"]  
+list1 = ["a", "b", "a"]                   
+list2 = ["a", "b", "c", "d"]  
 
-# copy_list1 = list1.copy()
-# copy_list1.reverse()
+copy_list1 = list1.copy()
+copy_list1.reverse()
 
-# if(copy_list1 == list1):
-#     print("palindrom")
-# else:
-#     print("Not Palindrome")
+if(copy_list1 == list1):
+    print("palindrom")
+else:
+    print("Not Palindrome")
 
 
 
